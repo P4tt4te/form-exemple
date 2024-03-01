be4c78a656c8bc8a49cdddcc3f36a70480fd6fa7
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2024-03-01 16:02:27 +0100

    feat: add SSR form errors

--- netlify/edge-functions/form-submission/form-submission.ts
+++ netlify/edge-functions/form-submission/form-submission.ts
@@ -1,7 +1,9 @@
 import type { Context } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
+  console.log("resquest", request);
   const response = await context.next();
+  console.log("response", response);
   response.headers.set("X-Your-Custom-Header", "A custom value");
   return response;
 };
